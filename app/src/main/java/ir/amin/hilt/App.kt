ec5d96dd7c1abc7f9e5c01c4666b4ccda3663c0f
Amin Rooholamini <nomad.cr34@gmail.com>
2021-02-02 20:01:52 +0330

    provide dependency for interface or third libraries

---
+++ app/src/main/java/ir/amin/hilt/App.kt
@@ -0,0 +1,11 @@
+package ir.amin.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+
+
+
+}
---
+++ app/src/main/java/ir/amin/hilt/MainActivity.kt
@@ -0,0 +1,81 @@
+package ir.amin.hilt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var someClass: SomeClass
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println(someClass.doAThing())
+    }
+}
+
+class SomeClass
+@Inject
+constructor(
+    private val someInterface: SomeInterface,
+    private val gson: Gson
+) {
+    fun doAThing(): String {
+        return "Look I got: ${someInterface.getThing()}"
+    }
+}
+
+class SomeInterfaceImpl
+@Inject
+constructor(
+    private val someDependency: String
+) : SomeInterface {
+    override fun getThing(): String {
+        return "A thing $someDependency"
+    }
+
+}
+
+interface SomeInterface {
+    fun getThing(): String
+}
+
+@InstallIn(SingletonComponent::class)//above activity
+@Module
+class MyModule{
+
+    @Singleton
+    @Provides
+    fun provideSomeString():String{
+        return "some String"
+    }
+
+    @Singleton
+    @Provides
+    fun provideInterfaceImpl(
+        someString:String
+    ):SomeInterface{
+        return SomeInterfaceImpl(someString)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson():Gson{
+        return Gson()
+    }
+
+
+}
+
+
